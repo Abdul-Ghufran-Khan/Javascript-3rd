6b9e9ac8f79ce993645fdfda5a6aecfb87f65d28
Abdul-Ghufran-Khan <abdulghufran267@gmail.com>
2024-05-17 19:08:22 +0500

    Completed

---
+++ app.js
@@ -0,0 +1,17 @@
+  // Question 1
+  var age = 20
+  alert("I am "+age+" years old")
+  // Question 2
+  var visited = 14
+  alert("You have visited this site "+visited+" times")
+
+  // Question 3
+  var  birthYear = 2004
+  document.write("My birth year is "+birthYear)
+
+  // Question 4
+  var website ="XYZ Clothing store"
+  var visitor_Name = "Abdul Ghufran"
+  var quantity = "5"
+  var product = "T-shirt"
+  document.write("<p>"+"<b>"+visitor_Name+"</b>"+" ordered "+"<b>"+quantity+" "+product+"</b>"+" on "+website+"</p>");
